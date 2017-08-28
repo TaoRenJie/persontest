ac373b064baca26a73c30c700e5a787238cbfbd7
TaoRenJie <1019987682@qq.com>
2017-08-28 16:23:00 +0800

    完成图片排布，需要添加SDWEBIMAGE

--- PersonalTest/MyTableViewCell.swift
+++ PersonalTest/MyTableViewCell.swift
@@ -20,12 +20,11 @@ class MyTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        setupUI()
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        setupUI()
         // Configure the view for the selected state
     }
 
@@ -38,6 +37,7 @@ class MyTableViewCell: UITableViewCell {
         let data = try! Data(contentsOf: url)
         self.headImageView.image = UIImage(data:data)
         self.pictureArray = model.pictureArray
+        setupUI()
     }
 
     func setupUI() {
@@ -48,16 +48,48 @@ class MyTableViewCell: UITableViewCell {
         } else if self.pictureArray.count >= 1 && self.pictureArray.count <= 3 {
             self.pictureViewheightConstraint.constant = 30 + imageViewHeight
             for count in 0 ..< self.pictureArray.count  {
-                for pictureName in pictureArray {
-                    let imageView = UIImageView()
-                    imageView.frame = CGRect(x:CGFloat(15*(count+1) + count * Int(imageViewHeight)), y:15, width:imageViewHeight, height:imageViewHeight)
-                    guard let url = URL(string: pictureName) else {
-                        return
-                    }
-                    let data = try! Data(contentsOf: url)
-                    imageView.image = UIImage(data:data)
-                    self.pictureView?.addSubview(imageView)
+                let imageView = UIImageView()
+                imageView.frame = CGRect(x:CGFloat(15*(count+1) + count * Int(imageViewHeight)), y:15, width:imageViewHeight, height:imageViewHeight)
+                guard let url = URL(string:pictureArray[count]) else {
+                    return
                 }
+                let data = try! Data(contentsOf: url)
+                imageView.image = UIImage(data:data)
+                self.pictureView?.addSubview(imageView)
+            }
+        } else if self.pictureArray.count >= 4 && self.pictureArray.count <= 6 {
+            self.pictureViewheightConstraint.constant = 45 + 2*imageViewHeight
+            for count in 0 ..< self.pictureArray.count  {
+                let imageView = UIImageView()
+                if count >= 3 {
+                    imageView.frame = CGRect(x:CGFloat(15*(count-2) + (count-3) * Int(imageViewHeight)), y:30 + imageViewHeight, width:imageViewHeight, height:imageViewHeight)
+                } else {
+                    imageView.frame = CGRect(x:CGFloat(15*(count+1) + count * Int(imageViewHeight)), y:15, width:imageViewHeight, height:imageViewHeight)
+                }
+                guard let url = URL(string:pictureArray[count]) else {
+                    return
+                }
+                let data = try! Data(contentsOf: url)
+                imageView.image = UIImage(data:data)
+                self.pictureView?.addSubview(imageView)
+            }
+        } else if self.pictureArray.count >= 7 && self.pictureArray.count <= 9 {
+            self.pictureViewheightConstraint.constant = 60 + 3*imageViewHeight
+            for count in 0 ..< self.pictureArray.count  {
+                let imageView = UIImageView()
+                if count >= 6 {
+                    imageView.frame = CGRect(x:CGFloat(15*(count-5) + (count-6) * Int(imageViewHeight)), y:45 + 2*imageViewHeight, width:imageViewHeight, height:imageViewHeight)
+                } else if count >= 3 && count  <= 5 {
+                    imageView.frame = CGRect(x:CGFloat(15*(count-2) + (count-3) * Int(imageViewHeight)), y:30 + imageViewHeight, width:imageViewHeight, height:imageViewHeight)
+                } else {
+                    imageView.frame = CGRect(x:CGFloat(15*(count+1) + count * Int(imageViewHeight)), y:15, width:imageViewHeight, height:imageViewHeight)
+                }
+                guard let url = URL(string:pictureArray[count]) else {
+                    return
+                }
+                let data = try! Data(contentsOf: url)
+                imageView.image = UIImage(data:data)
+                self.pictureView?.addSubview(imageView)
             }
         }
     }
