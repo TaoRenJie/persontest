0ae4cc80630b1c4e4027197951053e944b9dad10
TaoRenJie <1019987682@qq.com>
2017-08-30 17:59:27 +0800

    添加按钮

--- PersonalTest/MyTableViewCell.swift
+++ PersonalTest/MyTableViewCell.swift
@@ -20,7 +20,6 @@ class MyTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
